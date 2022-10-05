226f6e76e53dad393d430ce6367c0165a7774795
elvis <gicharuelvis@gmail.com>
2022-10-05 09:30:26 +0300

    commit tree 7a887c8f4be39e19adbfc064dbf716a60e914ee5

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+const cats= ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat =function (name){
+    cats.push(name);
+}
+
+// const destructivelyAppendCat =function (){
+//     return cats.push('Ralph');
+// }
+
+const destructivelyPrependCat = ()=>{
+   return cats.unshift('Bob');
+};
+
+const destructivelyRemoveLastCat =()=>{
+    cats.pop();
+};
+
+const destructivelyRemoveFirstCat = function(){
+    cats.shift();
+};
+
+const appendCat = function(name){
+   return [...cats,name];
+};
+
+const prependCat = function(name){
+    return [name,...cats];
+ };
+
+// const removeLastCat = ()=>{
+//     return [...cats.slice(0,2)];
+//  };
+
+const removeLastCat = ()=>{
+    return [...cats.slice(0, cats.length - 1)];
+ };
+
+
+const removeFirstCat=()=>{
+    return [...cats.slice(1)]
+};
+
